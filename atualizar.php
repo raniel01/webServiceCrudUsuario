634d160c20846a62551087e2a10c673aca94ad86
raniel01 <ranielsnts@hotmail.com>
2019-10-18 20:50:58 -0300

    atualizando

--- atualizar.php
+++ atualizar.php
@@ -3,7 +3,7 @@ include('conn.php');
 
 $nome = $_POST['nome'];
 $email = $_POST['email'];
-
+$senha = md5($_POST['senha']);
 $codigo = $_POST['codigo'];
 
 $query = "update tb_usuario set nm_usuario = '$nome', ds_email = '$email', ds_senha = '$senha' where cd_usuario = $codigo";
